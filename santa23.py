59c875fae70e0cc0fcd05e87cbc58e0c8dd9d26c
joe.hirose <joe.hirose@brainpad.co.jp>
2023-12-20 12:44:45 +0900

    fix and test create

--- santa23.py
+++ santa23.py
@@ -2,37 +2,80 @@
 
 class SymmetricGroup():
 
-
     def __init__(self, perm):
-        
+
+        '''
+        perm : List
+        '''
+
         self.perm = perm
+        if type(self.perm)!=list:
+            raise ValueError('permutation should be list.')
         if sorted(self.perm)!=list(range(len(perm))):
             raise ValueError('perm should be continuous.')
 
-    def __len__(self):
-        return len(self.perm)
-
     def __str__(self):
+
         return str(self.perm)
 
     def __call__(self, x):
-        if len(x)!=len(self):
-            raise ValueError('same length only.')
-        return [x[t] for t in self.perm]
+
+        '''
+        return same type according to x. (list and SymmetricGroup only so far.)
+        '''
+
+        if type(x)==SymmetricGroup:
+            if len(x.perm)!=len(self.perm):
+                raise ValueError('same length only.')
+            _perm = [x.perm[t] for t in self.perm]
+            return SymmetricGroup(_perm)
+        if type(x)==list:
+            if len(x)!=len(self.perm):
+                raise ValueError('same length only.')
+            _perm = [x[t] for t in self.perm]
+        return _perm
 
     def __neg__(self):
+
+        '''
+        return negative element.
+        '''
+
         _perm_dict = dict(zip(self.perm,list(range(len(self.perm)))))
         _perm = [num for _, num in sorted(_perm_dict.items())]
         return SymmetricGroup(_perm)
     
     def __mul__(self, other):
+
+        '''
+        same type only
+        '''
+
+        return self(other)
+    
+    def __imul__(self, other):
+
+        '''
+        same type only
+        '''
+
         _perm = self(other.perm)
-        return SymmetricGroup(_perm)
+        self.perm = _perm
+
+        return self
 
     def __eq__(self, other):
-        return self.perm==other.perm
+
+        if type(self)==type(other):
+            return self.perm==other.perm
+        else:
+            return False
     
     def __pow__(self, num):
+
+        '''
+        num should be integer.
+        '''
 
         if type(num)!=int:
             raise ValueError('power should be integer.')
@@ -44,3 +87,30 @@ class SymmetricGroup():
             else:
                 _perm = eval('_unit'+' * (-self)'*(-num))
                 return _perm
+
+    def copy(self):
+
+        '''
+        duplicate oneself
+        '''
+
+        return SymmetricGroup(self.perm)
+
+    def __len__(self):
+
+        '''
+        here len means by multipling how many itselves this instance is reobtained.
+        
+        self**len(self) == unit
+        self**(len(self)+1) == self
+        '''
+
+        _perm = self.copy()
+        k = 0
+        while True:
+            _perm *= self.copy()
+            k += 1
+            if _perm == self.copy():
+                break
+            
+        return k
---
+++ santa23_test.py
@@ -0,0 +1,5 @@
+
+class SymmetricGroupTest():
+
+    def __init__(sefl):
+        pass
